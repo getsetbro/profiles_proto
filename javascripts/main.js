d4732fc667b9bbd8b96a1635af4834c616f20802
Seth Broweleit <hello@getsetbro.com>
2014-03-17 17:06:35 -0500

    waseem changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,17 +2,21 @@ var app = (function() {
   "use strict";
 
   //category select changes
-  var $cat_chx, $cat_rdo;
 
-  $cat_chx = $("[name=\"cat-x\"]");
-  $cat_rdo = $("[name=\"cat-y\"]");
+  var $cat_chx = $('[name="cat-x"]');
+  var $cat_rdo = $('[name="cat-y"]');
+  var $bdy = $(document.body);
 
   $cat_chx.change(function(e) {
-  $cat_rdo.prop("checked", false);
+    $cat_rdo.prop("checked", false);
+    $bdy.addClass('chk');
+    $bdy.removeClass('owner');
   });
 
   $cat_rdo.change(function(e) {
-  $cat_chx.prop("checked", false);
+    $cat_chx.prop("checked", false);
+    $bdy.addClass('owner');
+    $bdy.removeClass('chk');
   });
 
   return {
