23ce638cd50add84a80f8d045688a17d5a1dd939
Seth Broweleit <hello@getsetbro.com>
2014-03-17 11:51:19 -0500

    v1

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,21 @@
-console.log('This would be the main JS file.');
+var app = (function() {
+  "use strict";
+
+  //category select changes
+  var $cat_chx, $cat_rdo;
+
+  $cat_chx = $("[name=\"cat-x\"]");
+  $cat_rdo = $("[name=\"cat-y\"]");
+
+  $cat_chx.change(function(e) {
+  $cat_rdo.prop("checked", false);
+  });
+
+  $cat_rdo.change(function(e) {
+  $cat_chx.prop("checked", false);
+  });
+
+  return {
+    a: 0
+  };
+})();
