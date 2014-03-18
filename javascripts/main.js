f38b756b8b656946a7a2363ba718d3af51f141e4
Seth Broweleit <hello@getsetbro.com>
2014-03-18 10:06:23 -0500

    hide show

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,22 +1,30 @@
 var app = (function() {
   "use strict";
 
-  //category select changes
+  var $bdy = $(document.body);
 
   var $cat_chx = $('[name="cat-x"]');
   var $cat_rdo = $('[name="cat-y"]');
-  var $bdy = $(document.body);
 
+  var $debt =    $('#debt');
+  var $str_fin = $('#str_fin');
+  var $owner =   $('#owner');
+
+  //category select changes
   $cat_chx.change(function(e) {
-    $cat_rdo.prop("checked", false);
-    $bdy.addClass('chk');
-    $bdy.removeClass('owner');
+    if(e.target.checked){
+      $cat_rdo.prop("checked", false);
+      $bdy.addClass(e.target.id);
+      $bdy.removeClass('owner');
+    }else{
+      $bdy.removeClass(e.target.id);
+    }
   });
 
   $cat_rdo.change(function(e) {
     $cat_chx.prop("checked", false);
-    $bdy.addClass('owner');
-    $bdy.removeClass('chk');
+    $bdy.attr("class", 'owner');
+
   });
 
   return {
